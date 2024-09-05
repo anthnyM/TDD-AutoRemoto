d6559ec963884699ab4b141adc73a0e536b9656d
antonio <antonio.ontiveros@ucb.edu.bo>
2024-09-04 21:19:47 -0400

    Test 5: Validar comando

--- src/auto.js
+++ src/auto.js
@@ -1,5 +1,8 @@
 function leer_comando(com){
-    return com
+    if(validar_comando(com))
+        return com
+    else
+        return "Comando invalido"
 }
 
 function leer_dimension(com){
@@ -15,8 +18,22 @@ function leer_pos_inicial(com){
 
 function leer_instrucciones(com){
     let slash = com.lastIndexOf('/')
-    let short = com.slice(slash)
     return com.slice(slash + 1)
 }
 
-export {leer_comando, leer_dimension, leer_pos_inicial, leer_instrucciones}
+function validar_comando(com){
+    let dim = leer_dimension(com)
+    let posi = leer_pos_inicial(com)
+    let inst = leer_instrucciones(com)
+
+    let val_dim = /^\d+,\d+$/
+    let val_posi = /^\d+,\d+[NSEO]$/
+    let val_inst = /^[AID]+$/
+
+    if (val_dim.test(dim) && val_posi.test(posi) && val_inst.test(inst))
+        return true
+    else
+        return false
+}
+
+export {leer_comando, leer_dimension, leer_pos_inicial, leer_instrucciones, validar_comando}
--- src/auto.test.js
+++ src/auto.test.js
@@ -1,20 +1,30 @@
-import { leer_comando , leer_dimension, leer_instrucciones, leer_pos_inicial} from "./auto.js";
+import { leer_comando , leer_dimension, leer_instrucciones, leer_pos_inicial, validar_comando} from "./auto.js";
 
 describe("Auto a control remoto", () => {
     it("deberia validar el comando 5,5/1,2N/IAIAIAIAA", () => {
         expect(leer_comando("5,5/1,2N/IAIAIAIAA")).toEqual("5,5/1,2N/IAIAIAIAA");
-      });
+    });
+    it("deberia retornar Comando invalido", () => {
+        expect(leer_comando("5,5/1,2/IAI")).toEqual("Comando invalido");
+    });
     it("deberia retornar la dimension del mapa", () => {
        expect(leer_dimension("5,5/1,2N/IAIAIAIAA")).toEqual("5,5");
     });
-    it("deberia retornar la posicion inicial 1,2", () => {
+    it("deberia retornar la posicion inicial 1,2N", () => {
         expect(leer_pos_inicial("5,5/1,2N/IAIAIAIAA")).toEqual("1,2N");
     });
-    it("deberia retornar las instrucciones 6,6/2,1O/AIADAIA", () => {
+    it("deberia retornar las instrucciones AIADAIA", () => {
       expect(leer_instrucciones("6,6/2,1O/AIADAIA")).toEqual("AIADAIA");
     });
     it("deberia retornar las instrucciones IAIAIAIAA", () => {
       expect(leer_instrucciones("5,5/1,2N/IAIAIAIAA")).toEqual("IAIAIAIAA");
     });
+    it("deberia retornar true si el comando es valido", () => {
+        expect(validar_comando("5,5/1,2N/IAIAIAIAA")).toEqual(true);
+    });
+    it("deberia retornar false si el comando es invalido", () => {
+        expect(validar_comando("5,5/1,2N/zdns")).toEqual(false);
+    });
+      
     
 })
