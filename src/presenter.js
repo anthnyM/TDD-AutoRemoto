d55b93563dba45368571df39b5acdbba0a5fe2b8
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-09-04 21:10:45 -0400

    Agregar UI: Mostrar instrucciones

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import { leer_comando, leer_dimension, leer_pos_inicial} from "./auto.js";
+import { leer_comando, leer_dimension, leer_pos_inicial, leer_instrucciones} from "./auto";
 
 const comando = document.querySelector("#comando");
 const form = document.querySelector("#comando-form");
@@ -10,9 +10,11 @@ form.addEventListener("submit", (event) => {
   const output = leer_comando(comando.value);
   const dimension = leer_dimension(comando.value);
   const pos_inicial = leer_pos_inicial(comando.value);
+  const instrucciones = leer_instrucciones(comando.value)
 
   div.innerHTML = 
   "<p>Comando: " + output + "</p>" +
   "<p>Dimension: " + dimension + "</p>" +
-  "<p>Posicion inicial: " + pos_inicial + "</p>";
+  "<p>Posicion inicial: " + pos_inicial + "</p>" +
+  "<p>Instrucciones: " + instrucciones + "</p>";
 });
