2ac379f8d7ede63dfa796a577ad8e4f1b6c7c1f6
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-09-04 22:06:44 -0400

    Add UI: Mostrar posicion final

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import { leer_comando, leer_dimension, leer_pos_inicial, leer_instrucciones, validar_comando} from "./auto";
+import { leer_comando, leer_dimension, leer_pos_inicial, leer_instrucciones, validar_comando, moverAuto} from "./auto.js";
 
 const comando = document.querySelector("#comando");
 const form = document.querySelector("#comando-form");
@@ -10,14 +10,17 @@ form.addEventListener("submit", (event) => {
   const output = leer_comando(comando.value);
   const dimension = leer_dimension(comando.value);
   const pos_inicial = leer_pos_inicial(comando.value);
-  const instrucciones = leer_instrucciones(comando.value)
+  const instrucciones = leer_instrucciones(comando.value);
+  const pos_final = moverAuto(dimension, pos_inicial, instrucciones)
 
   if (validar_comando(comando.value)){
     div.innerHTML = 
     "<p>Comando: " + output + "</p>" +
     "<p>Dimension: " + dimension + "</p>" +
     "<p>Posicion inicial: " + pos_inicial + "</p>" +
-    "<p>Instrucciones: " + instrucciones + "</p>";
+    "<p>Instrucciones: " + instrucciones + "</p>" + 
+    "<p>Posicion Final: " + pos_final + "</p>";
+
   }
   else {
     div.innerHTML = "<p>Comando invalido</p>"
